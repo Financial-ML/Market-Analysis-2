5ec05b53bf5c91dd404a4be6a2c59ebee475dc53
amjad-aburaya <amjadsaed502@gmail.com>
2019-08-04 23:29:18 +0300

    ML
    Co-Authored-By: aburaya <aburaya@users.noreply.github.com>

---
+++ Feature-Calculator/collector.py
@@ -0,0 +1,144 @@
+import pandas as pd
+from features import *
+
+data = pd.read_csv('Name.csv')
+data.columns = ['date','open','high','low','close','volume']
+data = data.set_index(pd.to_datetime(data.date))
+data = data[['open','high','low','close','volume']]
+prices = data.drop_duplicates(keep=False)
+
+momentumKey = [3,4,5,8,9,10] 
+stochasticKey = [3,4,5,8,9,10] 
+williamsKey = [6,7,8,9,10] 
+procKey = [12,13,14,15] 
+wadlKey = [15] 
+adoscKey = [2,3,4,5] 
+macdKey = [15,30] 
+cciKey = [15] 
+bollingerKey = [15] 
+heikenashiKey = [15] 
+paverageKey = [2] 
+slopeKey = [3,4,5,10,20,30] 
+fourierKey = [10,20,30] 
+sineKey = [5,6] 
+marketKey = [1]
+
+
+keylist = [momentumKey,stochasticKey,williamsKey,procKey,wadlKey,adoscKey,macdKey,cciKey,bollingerKey, heikenashiKey
+           ,paverageKey,slopeKey,fourierKey,sineKey,marketKey] 
+
+momentumDict = momentum(prices,momentumKey) 
+print('1') 
+stochasticDict = stochastic(prices,stochasticKey) 
+print('2') 
+williamsDict = williams(prices,williamsKey)
+print('3') 
+procDict = proc(prices,procKey) 
+print('4') 
+wadlDict = wadl(prices,wadlKey) 
+print('5')
+adoscDict = adosc(prices,adoscKey)
+print('6') 
+macdDict = macd(prices,macdKey) 
+print('7') 
+cciDict = cci(prices,cciKey) 
+print('8')
+bollingerDict = bollinger(prices,bollingerKey,2) 
+print('9') 
+
+hkaprices = prices.copy()
+hkaprices['Symbol']='SYMB'
+HKA = OHLCresample(hkaprices,'15H')
+
+heikenDict = Heiken_Ashi(HKA,heikenashiKey) 
+print('10' ) 
+paverageDict = pavarage(prices,paverageKey) 
+print('11') 
+slopeDict = slopes(prices,slopeKey) 
+print('12') 
+fourierDict = fourier(prices,fourierKey) 
+print('13') 
+sineDict = sine(prices,sineKey) 
+print('14') 
+marketDict = Market(prices,marketKey) 
+print('15') 
+# Create list of dictionaries 
+
+dictlist = [momentumDict.close,stochasticDict.close,williamsDict.close
+            ,procDict.proc,wadlDict.wadl,adoscDict.AD,macdDict.line
+            ,cciDict.cci,bollingerDict.bands,heikenDict.candles,paverageDict.avs
+            ,slopeDict.slope,fourierDict.coeffs,sineDict.coeffs,marketDict.slope] 
+
+# list of column name on csv
+
+colFeat = ['momentum','stoch','will','proc','wadl','adosc','macd',
+           'cci','bollinger','heiken','paverage','slope','fourier','sine','market']
+
+
+masterFrame = pd.DataFrame(index = prices.index) 
+for i in range(0,len(dictlist)): 
+    if colFeat[i] == 'macd':
+        colID = colFeat[i] + str(keylist[6][0]) + str(keylist[6][1]) 
+        masterFrame[colID] = dictlist[i] 
+    else: 
+        for j in keylist[i]: 
+            for k in list(dictlist[i][j]):
+                colID = colFeat[i] + str(j) + str(k)
+                masterFrame[colID] = dictlist[i][j][k]
+
+
+threshold = round(0.7*len(masterFrame)) 
+masterFrame[['open','high','low','close']] = prices[['open','high','low','close']]
+
+masterFrame.heiken150 = masterFrame.heiken150.fillna(method='bfill') 
+masterFrame.heiken151 = masterFrame.heiken151.fillna(method='bfill')
+masterFrame.heiken152 = masterFrame.heiken152.fillna(method='bfill')
+masterFrame.heiken153 = masterFrame.heiken153.fillna(method='bfill') 
+
+# Drop columns that have 30% or more NAN data 
+
+masterFrameCleaned = masterFrame.copy() 
+
+masterFrameCleaned = masterFrameCleaned.dropna(axis=1,thresh=threshold)
+masterFrameCleaned = masterFrameCleaned.dropna(axis=0)
+masterFrameCleaned.to_csv('calculated.csv')
+print('completed')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Feature-Calculator/features.py
@@ -0,0 +1,463 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+import scipy.optimize
+from scipy.optimize import OptimizeWarning
+import warnings
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+#------------------------------------------------------------------------------
+class Holder:
+    1
+#------------------------------------------------------------------------------
+
+def Heiken_Ashi(price,period):
+    result = Holder()
+    dict = {}
+    HAclose = price[['open','high','low','close']].sum(axis=1)/4
+    HAopen = HAclose.copy()
+    HAopen.iloc[0]=HAclose.iloc[0]
+    HAhigh = HAclose.copy()
+    HAlow = HAclose.copy()  
+    for i in range(1,len(price)):
+        HAopen.iloc[i] = (HAopen.iloc[i-1]+HAclose.iloc[i-1])/2
+        HAhigh.iloc[i] = np.array([price.high.iloc[i],HAopen.iloc[i],HAclose.iloc[i]]).max()
+        HAlow.iloc[i] = np.array([price.low.iloc[i],HAopen.iloc[i],HAclose.iloc[i]]).min()
+    df = pd.concat((HAopen,HAhigh,HAlow,HAclose),axis=1)
+    df.coulmns = ['open','high','low','close']
+    
+    df.index = df.index.droplevel(0)
+    
+    dict[period[0]]=df
+    result.candles=dict
+    return result
+#------------------------------------------------------------------------------
+def detrend(price,method='difference'):
+    if method=='difference':
+        detrended=price.close[1:]-price.close[:-1].values
+    elif method=='linear':
+        x = np.arange(0,len(price))
+        y = price.close.values
+        model = LinearRegression()
+        model.fit(x.reshape(-1,1),y.reshape(-1,1))
+        trend = model.predict(x.reshape(-1,1))
+        trend = trend.reshape((len(price),))
+        detrended = price.close - trend
+    else:
+        print("error trend")
+    return detrended
+#------------------------------------------------------------------------------
+def fseries(x,a0,a1,b1,w):
+    f = a0 + a1 * np.cos(w * x) + b1 * np.sin(w * x)
+    return f
+#------------------------------------------------------------------------------
+def sseries(x,a0,b1,w):
+    f = a0 + b1 * np.sin(w * x)
+    return f
+#------------------------------------------------------------------------------
+def fourier(price,period,method='difference'):
+    results = Holder()
+    dict = {}
+    plot = False
+    detrended = detrend(price,method)
+    
+    for i in range(0,len(period)):
+        coeffs =[]
+        for j in range(period[i],len(price)-period[i]):
+            x = np.arange(0,period[i])
+            y = detrended.iloc[j-period[i]:j]
+            with warnings.catch_warnings():
+                warnings.simplefilter('error',OptimizeWarning)
+                
+                try:
+                    res = scipy.optimize.curve_fit(fseries,x,y)
+                    
+                except (RuntimeError,OptimizeWarning):
+                    res = np.empty((1,4))
+                    res[0,:] = np.NAN
+                
+            if plot == True:
+                xt = np.linspace(0,period[i],100)
+                yt = fseries(xt,res[0][0],res[0][1],res[0][2],res[0][3])
+                
+                plt.plot(x,y)
+                plt.plot(xt,yt,'r')
+                
+                plt.show()
+                
+            coeffs = np.append(coeffs,res[0],axis=0)
+            
+        warnings.filterwarnings('ignore',category=np.VisibleDeprecationWarning)
+        
+        coeffs = np.array(coeffs).reshape(((len(coeffs)//4,4)))
+        df = pd.DataFrame(coeffs,index=price.iloc[period[i]:-period[i]].index)
+        df.columns = ['a0','a1','b1','w']
+        df = df.fillna(method='bfill')
+        dict[period[i]] = df
+    results.coeffs = dict
+    return results
+#------------------------------------------------------------------------------
+def sine(price,period,method='difference'):
+    results = Holder()
+    dict = {}
+    plot = False
+    detrended = detrend(price,method)
+    
+    for i in range(0,len(period)):
+        coeffs =[]
+        for j in range(period[i],len(price)-period[i]):
+            x = np.arange(0,period[i])
+            y = detrended.iloc[j-period[i]:j]
+            with warnings.catch_warnings():
+                warnings.simplefilter('error',OptimizeWarning)
+                
+                try:
+                    res = scipy.optimize.curve_fit(sseries,x,y)
+                    
+                except (RuntimeError,OptimizeWarning):
+                    res = np.empty((1,3))
+                    res[0,:] = np.NAN
+                
+            if plot == True:
+                xt = np.linspace(0,period[i],100)
+                yt = sseries(xt,res[0][0],res[0][1],res[0][2])
+                
+                plt.plot(x,y)
+                plt.plot(xt,yt,'r')
+                
+                plt.show()
+                
+            coeffs = np.append(coeffs,res[0],axis=0)
+            
+        warnings.filterwarnings('ignore',category=np.VisibleDeprecationWarning)
+        
+        coeffs = np.array(coeffs).reshape(((len(coeffs)//3,3)))
+        df = pd.DataFrame(coeffs,index=price.iloc[period[i]:-period[i]].index)
+        df.columns = ['a0','b1','w']
+        df =df.fillna(method='bfill')
+        dict[period[i]]=df
+    results.coeffs = dict
+    return results
+#------------------------------------------------------------------------------
+def wadl(price,period):
+    
+    results = Holder()
+    dict = {}
+    
+    for i in range(0,len(period)):
+        
+        WAD = []
+        
+        for j in range(period[i],len(price)-period[i]):
+            
+            TRH = np.array([price.high.iloc[j],price.close.iloc[j-1]]).max()
+            TRL = np.array([price.low.iloc[j],price.close.iloc[j-1]]).min()
+            
+            if price.close.iloc[j] > price.close.iloc[j-1]:
+                PM = price.close.iloc[j]-TRL
+            elif price.close.iloc[j] < price.close.iloc[j-1]:
+                PM = price.close.iloc[j]-TRH
+            elif price.close.iloc[j] == price.close.iloc[j-1]:
+                PM = 0
+            else:
+                print("error in wadl")
+                
+            AD = PM * price.volume.iloc[j]
+            WAD = np.append(WAD,AD)
+            
+        WAD = WAD.cumsum()
+        WAD = pd.DataFrame(WAD,index=price.iloc[period[i]:-period[i]].index)
+        WAD.columns = ['close']
+        dict[period[i]] = WAD
+        
+    results.wadl = dict
+    return results
+#------------------------------------------------------------------------------
+
+def OHLCresample(DataFrame,TimeFrame,column='ask'):
+    grouped = DataFrame.groupby('Symbol')
+    
+    if np.any(DataFrame.columns == 'Ask'): 
+        
+        if column =='ask':
+            ask = grouped['Ask'].resample(TimeFrame).ohlc()
+            askVol = grouped['AskVol'].resample(TimeFrame).count()
+            resampled = pd.DataFrame(ask)
+            resampled['AskVol'] = askVol
+            
+        elif column =='bid':
+            bid = grouped['Bid'].resample(TimeFrame).ohlc()
+            bidVol = grouped['BidVol'].resample(TimeFrame).count()
+            resampled = pd.DataFrame(bid)
+            resampled['BidVol'] = bidVol
+            
+        else:
+            raise ValueError('error OHLCresample')
+            
+    elif np.any(DataFrame.columns == 'close'):
+        open = grouped['open'].resample(TimeFrame).ohlc()
+        close = grouped['close'].resample(TimeFrame).ohlc()
+        high = grouped['high'].resample(TimeFrame).ohlc()
+        low = grouped['low'].resample(TimeFrame).ohlc()
+        askVol = grouped['volume'].resample(TimeFrame).count() 
+    
+        resampled = pd.DataFrame(open) 
+        resampled['high'] = high 
+        resampled['low'] = low 
+        resampled['close'] = close 
+        resampled['volume'] = askVol 
+                  
+    resampled = resampled.dropna()
+    
+    return resampled
+#------------------------------------------------------------------------------
+def momentum(prices,periods):
+    
+    results = Holder() 
+    open = {} 
+    close = {} 
+    for i in range(0,len(periods)): 
+        
+        open[periods[i]] = pd.DataFrame(prices.open.iloc[periods[i]:]-prices.open.iloc[:-periods[i]].values, index=prices.iloc[periods[i]:].index) 
+        close[periods[i]] = pd.DataFrame(prices.close.iloc[periods[i]:] - prices.close.iloc[:-periods[i]].values, index=prices.iloc[periods[i]:].index) 
+        
+        open[periods[i]].columns = ['open']
+        close[periods[i]].columns = ['close'] 
+    
+    results.open = open 
+    results.close = close 
+    
+    return results 
+
+#------------------------------------------------------------------------------
+
+def stochastic(prices,periods):
+    results = Holder() 
+    close = {} 
+    for i in range(0,len(periods)):
+        Ks=[]
+        for j in range(periods[i],len(prices)-periods[i]):
+            C= prices.close.iloc[j+1] 
+            H = prices.high.iloc[j-periods[i]:j].max() 
+            L= prices.low.iloc[j-periods[i]:j].min() 
+            if H == L:
+                K = 0 
+            else: 
+                K= 100*(C-L)/(H-L) 
+            Ks = np.append(Ks,K)
+            
+        df = pd.DataFrame(Ks,index =prices.iloc[periods[i]+1:-periods[i]+1].index) 
+        df.columns = ['K']
+        df['D'] = df.K.rolling(3).mean() 
+        df = df.dropna() 
+        
+        close[periods[i]] = df 
+        
+    results.close = close 
+        
+    return results 
+
+#------------------------------------------------------------------------------
+
+def williams(prices,periods):
+    results = Holder() 
+    close = {} 
+    for i in range(0,len(periods)):
+        Rs=[]
+        for j in range(periods[i],len(prices)-periods[i]):
+            
+            C= prices.close.iloc[j+1] 
+            H = prices.high.iloc[j-periods[i]:j].max() 
+            L= prices.low.iloc[j-periods[i]:j].min() 
+            if H == L:
+                R = 0 
+            else: 
+                R= 100*(H-C)/(H-L) 
+            Rs = np.append(Rs,R)
+            
+        df = pd.DataFrame(Rs,index =prices.iloc[periods[i]+1:-periods[i]+1].index) 
+        df.columns = ['R']
+        df = df.dropna() 
+        
+        close[periods[i]] = df 
+        
+    results.close = close 
+        
+    return results 
+
+#------------------------------------------------------------------------------
+
+def proc(prices,periods):
+    results = Holder() 
+    proc = {} 
+    for i in range(0,len(periods)):
+        proc[periods[i]] = pd.DataFrame((prices.close.iloc[periods[i]:] - prices.close.iloc[:-periods[i]].values)/prices.close.iloc[:-periods[i]].values)
+        proc[periods[i]].columns = ['close']
+    
+    results.proc = proc
+    return results
+
+#------------------------------------------------------------------------------
+
+def adosc(prices,periods):
+    results = Holder() 
+    accdist = {} 
+    for i in range(0,len(periods)):
+        AD=[]
+        for j in range(periods[i],len(prices)-periods[i]):
+            
+            C = prices.close.iloc[j+1] 
+            H = prices.high.iloc[j-periods[i]:j].max() 
+            L = prices.low.iloc[j-periods[i]:j].min() 
+            V = prices.volume.iloc[j+1]
+            if H == L:
+                CLV = 0 
+            else: 
+                CLV= ((C-L)-(H-C))/(H-L) 
+            AD = np.append(AD,CLV*V)
+        AD = AD.cumsum()   
+        AD = pd.DataFrame(AD,index =prices.iloc[periods[i]+1:-periods[i]+1].index) 
+        AD.columns = ['AD']
+        
+        accdist[periods[i]] = AD 
+        
+    results.AD = accdist 
+        
+    return results 
+
+#------------------------------------------------------------------------------
+
+def macd(prices,periods):
+    
+    results = Holder() 
+    
+    EMA1 = prices.close.ewm(span=periods[0]).mean()
+    EMA2 = prices.close.ewm( span=periods[1]).mean()
+    
+    MACD = pd.DataFrame(EMA1 - EMA2)
+    MACD.columns = ['L'] 
+
+    sigMACD = MACD.rolling(3).mean() 
+    sigMACD.columns = ['SL'] 
+    
+    results.line = MACD 
+    results.signal = sigMACD 
+
+    return results
+
+#------------------------------------------------------------------------------
+    
+def cci (prices,periods):
+    results = Holder() 
+    CCI = {}
+
+    for i in range(0,len(periods)): 
+        MA = prices.close.rolling(periods[i]).mean() 
+        std = prices.close.rolling(periods[i]).std() 
+
+        D = (prices.close-MA)/std 
+        CCI[periods[i]] = pd.DataFrame((prices.close-MA)/(0.015*D)) 
+        CCI[periods[i]].columns = ['close'] 
+
+    results.cci = CCI 
+    
+    return results
+
+#------------------------------------------------------------------------------
+
+def bollinger (prices,periods,deviations):
+    results = Holder() 
+    boll = {}
+    for i in range(0,len(periods)): 
+        mid = prices.close.rolling(periods[i]).mean() 
+        std = prices.close.rolling(periods[i]).std() 
+
+        upper = mid+deviations*std
+        lower = mid-deviations*std
+        
+        df = pd.concat((upper,mid,lower),axis=1)
+        df.columns = ['upper','mid','lower']
+        
+        boll[periods[i]] = df
+        
+    results.bands = boll
+    return results
+
+#------------------------------------------------------------------------------
+    
+def pavarage (prices,periods):
+    results = Holder() 
+    avs = {}
+    for i in range(0,len(periods)): 
+        avs[periods[i]] = pd.DataFrame(prices[['open','high','low','close']].rolling(periods[i]).mean())
+        
+    results.avs = avs
+    return results
+
+#------------------------------------------------------------------------------
+
+def slopes (prices,periods):
+    results = Holder() 
+    slope = {}
+    
+    for i in range(0,len(periods)):
+        ms=[]
+        for j in range(periods[i],len(prices)-periods[i]):
+            y = prices.high.iloc[j-periods[i]:j].values
+            x = np.arange(0,len(y))
+            
+            res = stats.linregress(x,y=y)
+            m = res.slope
+            ms = np.append(ms,m)
+            
+        
+        ms = pd.DataFrame(ms,index=prices.iloc[periods[i]:-periods[i]].index)
+        ms.columns = ['high']
+        slope[periods[i]] = ms 
+        
+        
+    results.slope = slope 
+    
+    return results
+
+#------------------------------------------------------------------------------
+    
+def Market (prices,periods):
+    
+    results = Holder() 
+    slope = {}
+    
+    for i in range(0,len(periods)):
+        ms=[]
+        for j in range(periods[i],len(prices)-periods[i]):
+            if prices.open[j+1] < prices.close[j+1]:
+                ms.append(1)
+            elif prices.open[j+1] >= prices.close[j+1]:
+                ms.append(0)
+
+        ms = pd.DataFrame(ms,index=prices.iloc[periods[i]:-periods[i]].index)
+        ms.columns = ['Market']
+        slope[periods[i]] = ms 
+        
+        
+    results.slope = slope 
+    
+    return results
+
+#------------------------------------------------------------------------------
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
